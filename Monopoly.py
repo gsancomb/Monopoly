6cdf0c6b6f9e92bf9d0c5be124f1fa1f083a95d1
Griffin Sancomb <gsancom1@umbc.edu>
2020-02-10 18:07:25 -0500

    csv files for cards

--- Monopoly.py
+++ Monopoly.py
@@ -1,20 +1,30 @@
-#Author:    Griffin Sancomb
-#Date:  02/09/2020
+# Author:    Griffin Sancomb
+# Date:  02/09/2020
 import pandas as pd
 
-#constants
+
+# constants ints
 STARTER_MONEY = 1500
+DEFUALT_MULTI_UTIL = 2
+ADV_MULTI_UTIL = 10
+
+
+# constants str
+NO_OWNERSHIP = "You dont own any"
 
 
 class Player:
     def __init__(self, piece):
         self.piece = piece
         self.money = STARTER_MONEY
+        # self.sites = [][]
+        # self.station = []
+        # self.utilities = []
+        #
 
 
 def build_board():
     df_board = pd.read_csv("Monopoly_Board.csv")
-    print ()
     df_board["Owner"] = None
     df_board.loc[(df_board["Special"] == "site") | (df_board["Special"] == "utility") |
                  (df_board["Special"] == "station"), "Owner"] = "bank"
@@ -32,16 +42,24 @@ def build_board():
 #         players.append(new_player)
 
 def build_player():
-    player_df = pd.DataFrame(columns=["piece", "money"])
+    player_df = pd.DataFrame(columns=["piece", "money", "site", "station", "utilities"])
     num_players = input("Enter number of players from 2 - 8\n")
     for i in range(0, int(num_players)):
         player_piece = input("What piece would you like to be\n"
                              "Scottie dog      Top hat      Thimble       Boot\n"
                              "Wheelbarrow      Cat          Racing car    Battleship\n")
-        player_df = player_df.append({"piece": player_piece, "money": STARTER_MONEY},ignore_index=True)
+        player_df = player_df.append({"piece": player_piece, "money": STARTER_MONEY, "site": NO_OWNERSHIP,
+                                      "station": NO_OWNERSHIP, "utilities": NO_OWNERSHIP}, ignore_index=True)
     print()
+    
+
+def build_cards():
+    chance_df = pd.DataFrame(columns=["Description", "amount", "type", "tile", "house", "hotel"])
+    chest_df = pd.DataFrame(columns=["Description", "amount", "type", "house", "hotel"])
+
 
 
 if __name__ == '__main__':
-    #build_board()
+    build_board()
     build_player()
+    build_cards()
